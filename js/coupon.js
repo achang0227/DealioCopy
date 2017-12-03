48ea5ce391d49e9249a8a372b2785ce8a7c9c113
Adrienne Chang <ajaychang@Adriennes-MacBook-Pro.local>
2017-12-02 17:44:53 -0800

    starting addCoupon revisions

--- js/coupon.js
+++ js/coupon.js
@@ -419,7 +419,7 @@ function deleteCoupon(){
 $('#cancelDelete').click(function(){ //when click cancel
   $('#cancelDelete').hide(); //hides cancel option
   $('#cancelPlaceholder').show(); //shows the placeholder
-   $('#select').html("Select"); //changes delete to select
+   $('#select').html("Edit"); //changes delete to select
    // var z = document.getElementsByClassName('.checks')
    // $('.checks').hide();
    var z = document.getElementsByClassName('.form-check-input')
